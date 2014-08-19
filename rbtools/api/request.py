7bb8e7fb26075768dbcd1c6531b285a87f2ba920
Brett Higgins <brett.higgins@gmail.com>
2014-08-19 11:22:16 -0400

    Added more careful discovery of remote svn info, branch through subgit config.

--- rbtools/api/request.py
+++ rbtools/api/request.py
@@ -570,6 +570,8 @@ class ReviewBoardServer(object):
         """
         try:
             content_type, body = request.encode_multipart_formdata()
+            logging.debug('Request data: %r' % body)
+
             headers = request.headers
 
             if body:
